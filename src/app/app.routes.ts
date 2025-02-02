7968ac9b129fa2d3055c718d41429184799e4287
Kavinda Dissanayaka <92946@fhss.sjp.ac.lk>
2025-02-02 12:01:01 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { SkillComponent } from './Components/skill/skill.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path : "",
+    component : HomeComponent
+},
+{
+    path : "about",
+    component:AboutComponent
+},
+{
+    path: "contact",
+    component:ContactComponent
+},
+{
+    path:"skill",
+    component:SkillComponent
+}
+];
